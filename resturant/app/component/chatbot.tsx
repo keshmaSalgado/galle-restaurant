6f2f84814c11a2c6a2a961f983f693203e1e9c5f
Keshma Salgado <161168148+keshmaSalgado@users.noreply.github.com>
2026-04-23 19:39:13 +0530

    new updates

--- resturant/app/component/chatbot.tsx
+++ resturant/app/component/chatbot.tsx
@@ -16,7 +16,7 @@ export default function Chatbot() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch("https://resturant-backend-kjtc.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
